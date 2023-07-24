d4690d3799a40eb134500cf6cb54ef51b2030ab2
valery <vagechirkov@gmail.com>
2023-07-24 23:07:24 +0200

    add softmax belief

--- ice_fishing/ice_fishing_m1/belief.py
+++ ice_fishing/ice_fishing_m1/belief.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 from ice_fishing.ice_fishing_m1.utils.utils import draw_circe_around_point
 
@@ -8,6 +9,7 @@ class Belief:
         self.catch_rate = np.zeros((width, height))
         self.prior_info = np.zeros((width, height))
         self.social_info = np.zeros((width, height))
+        self.softmax_belief = np.zeros((width, height))
 
     def update_prior_info(self, locs: list[tuple[int, int]], radius: int = 3) -> None:
         self.prior_info = np.zeros_like(self.prior_info)
@@ -31,3 +33,11 @@ class Belief:
         self.catch_rate[loc] = catch_rate
         # make sure the catch rate is proper probability distribution
         self.catch_rate = self.catch_rate / self.catch_rate.sum()
+
+    def softmax_weighted_belief(self, weights: np.ndarray = (1/3, 1/3, 1/3), temperature: float = 0.1) -> None:
+        weighted_belief = weights[0] * self.prior_info + weights[1] * self.social_info + weights[2] * self.catch_rate
+
+        # softmax weighted belief with temperature
+        self.softmax_belief = scipy.special.softmax(weighted_belief / temperature)
+
+
--- ice_fishing/ice_fishing_m1/tests/test_belief.py
+++ ice_fishing/ice_fishing_m1/tests/test_belief.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from ice_fishing.ice_fishing_m1.belief import Belief
 
 
@@ -25,3 +27,19 @@ def test_update_prior_info():
     belief.update_prior_info([(50, 50), (70, 70)], radius=1)
 
     assert belief.prior_info.sum() == 10, "The prior info should be a circle of 1 around 50, 50 and 70, 70"
+
+
+def test_softmax_weighted_belief():
+    """
+    Test the softmax_weighted_belief method
+    """
+    belief = Belief(100, 100)
+
+    belief.update_prior_info([(50, 50)], radius=1)
+    belief.update_social_info([(50, 50)], radius=1)
+    belief.update_catch_rate((50, 50), 1)
+
+    belief.softmax_weighted_belief()
+
+    # check if the sum of the softmax belief is 1 with tolerance of 1e-5
+    assert np.isclose(belief.softmax_belief.sum(), 1, atol=1e-5), "The sum of the softmax belief should be 1"
