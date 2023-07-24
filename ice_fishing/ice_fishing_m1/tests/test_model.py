d2e0c4c9f537350d88a04ac4707fb0a7b30ddd18
valery <vagechirkov@gmail.com>
2023-07-24 14:32:07 +0200

    fix model test

--- ice_fishing/ice_fishing_m1/tests/test_model.py
+++ ice_fishing/ice_fishing_m1/tests/test_model.py
@@ -1,11 +1,19 @@
 import pytest
+from ice_fishing.ice_fishing_m1.model import IceFishingModel
 
 
 def tests_init():
-    from model import IceFishingModel
     model = IceFishingModel()
 
     assert model.n_agents == 5
     assert model.grid.width == 100
     assert model.grid.height == 100
-    assert model.running == True
+    assert model.running
+
+    assert model.schedule.agents[0].state == "initial"
+
+    # do one step
+    model.step()
+
+    assert model.schedule.agents[0].state == "moving"
+
